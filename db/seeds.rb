38b66bb12f5f4b93e0e15266c7ac4bc4946a7c8d
sarah bokobza <sarahbokobza@gmail.com>
2018-06-03 11:54:07 +0300

    changed navbar and home

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,17 @@
 # Cocktail.create(name: "Daiquiri")
 # Cocktail.create(name: "Bloody Mary")
 # Cocktail.create(name: "Gin&Tonic")
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient_list = JSON.parse(ingredient_serialized)
+ingredient = ingredient_list["drinks"]
+
+ingredient.each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
